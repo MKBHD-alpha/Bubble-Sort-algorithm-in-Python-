ed18b03ef490d199b073e422c0e98732f2883ff4
Deepraj Baidya <63138398+deepraj02@users.noreply.github.com>
2020-10-01 00:20:27 +0530

    Enable User Input
    Enabled taking Input from User (no limitations )

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,4 +1,15 @@
-a= [<Enter all your Numbers you want to sort>]
+n1=int(input("Enter Number 1:- "))
+n2=int(input("Enter Number 2:- "))
+n3=int(input("Enter Number 3:- "))
+n4=int(input("Enter Number 4:- "))
+n5=int(input("Enter Number 5:- "))
+n6=int(input("Enter Number 6:- "))
+n7=int(input("Enter Number 7:- "))
+n8=int(input("Enter Number 8:- "))
+n9=int(input("Enter Number 9:- "))
+n10=int(input("Enter Number 10:- "))
+
+a= [n1,n2,n3,n4,n5,n6,n7,n8,n9,n10]
 count=0
 for i in range(len(a)):
     for j in range(1,len(a)):
